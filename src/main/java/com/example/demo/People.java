017e6f6bfaa6521a88ae196a66483d7b480546b5
FranLuque <luquefran97@hotmail.com>
2022-05-30 17:20:04 +0200

    People.java modified

--- src/main/java/com/example/demo/People.java
+++ src/main/java/com/example/demo/People.java
@@ -1,8 +1,50 @@
 package com.example.demo;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.sql.Date;
 
 @Entity
+@Table(name = "people")
 public class People {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "height")
+    private Integer height;
+
+    @Column(name = "mass")
+    private Integer mass;
+
+    @Column(name = "hair_color")
+    private String hair_color;
+
+    @Column(name = "skin_color")
+    private String skin_color;
+
+    @Column(name = "eye_color")
+    private String eye_color;
+
+    @Column(name = "birth_year")
+    private String birth_year;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "planet_id")
+    private Integer planet_id;
+
+    @Column(name = "created_date")
+    private Date created_date;
+
+    @Column(name = "updated_date")
+    private Date updated_date;
+
+    @Column(name = "url")
+    private String url;
 
 }
