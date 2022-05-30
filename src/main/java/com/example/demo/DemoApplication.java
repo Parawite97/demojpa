2a00c61a29e0c9803c87268c4e9f39e039bede98
FranLuque <luquefran97@hotmail.com>
2022-05-30 18:26:09 +0200

    People.java modified, PeopleRepository.java created, DemoApplication.java modified

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Autowired
+	private PeopleRepository peopleRepository;
+
+	public void run(String... args) {
+		this.peopleRepository.findAll().stream().forEach(e -> System.out.println(e));
+
 	}
 
 }
--- src/main/java/com/example/demo/People.java
+++ src/main/java/com/example/demo/People.java
@@ -1,11 +1,12 @@
 package com.example.demo;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.sql.Date;
 
 @Entity
 @Table(name = "people")
-public class People {
+public class People implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -47,4 +48,115 @@ public class People {
     @Column(name = "url")
     private String url;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getMass() {
+        return mass;
+    }
+
+    public void setMass(Integer mass) {
+        this.mass = mass;
+    }
+
+    public String getHair_color() {
+        return hair_color;
+    }
+
+    public void setHair_color(String hair_color) {
+        this.hair_color = hair_color;
+    }
+
+    public String getSkin_color() {
+        return skin_color;
+    }
+
+    public void setSkin_color(String skin_color) {
+        this.skin_color = skin_color;
+    }
+
+    public String getEye_color() {
+        return eye_color;
+    }
+
+    public void setEye_color(String eye_color) {
+        this.eye_color = eye_color;
+    }
+
+    public String getBirth_year() {
+        return birth_year;
+    }
+
+    public void setBirth_year(String birth_year) {
+        this.birth_year = birth_year;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getPlanet_id() {
+        return planet_id;
+    }
+
+    public void setPlanet_id(Integer planet_id) {
+        this.planet_id = planet_id;
+    }
+
+    public Date getCreated_date() {
+        return created_date;
+    }
+
+    public void setCreated_date(Date created_date) {
+        this.created_date = created_date;
+    }
+
+    public Date getUpdated_date() {
+        return updated_date;
+    }
+
+    public void setUpdated_date(Date updated_date) {
+        this.updated_date = updated_date;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "People{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
